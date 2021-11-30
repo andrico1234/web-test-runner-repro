aa5c6863e3b558a889fb2aedcc306b3f445c7d21
Andrico <andrico.karoulla@healthhero.com>
2021-11-30 08:58:22 +0000

    create repro

---
+++ tests/lib/rules/no-inline-styles.js
@@ -0,0 +1,15 @@
+import path from "path";
+
+import rule from "../../../lib/rules/no-inline-styles.js";
+import { RuleTester } from "eslint";
+
+const ruleTester = new RuleTester({
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2015,
+  },
+});
+
+ruleTester.run("no-inline-styles", rule, {
+  valid: ['html`<div class="fake-name"></div>`'],
+});
